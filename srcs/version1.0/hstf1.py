1653eef9b83031780bcfc763dffec759828ef32d
fubar <fu@exqmale.com>
2018-06-15 13:20:09 -0400

    fuck sci notation

--- srcs/version1.0/hstf1.py
+++ srcs/version1.0/hstf1.py
@@ -1,11 +1,19 @@
+import numpy as np
 import ccxt
 import os
 
-def get_data(currencies):
+def getNum(str):
+    tmp = ""
+    for i, l in enumerate(str):
+        if l.isnumeric() or l == ".":
+            tmp += l
+    return float(tmp)
+
+def get_data(currencies, interval):
     data = []
     for i, currency in enumerate(currencies):
         quote = currency.split('/')[0]
-        filename = f'../../data/{quote}_5m_OHLCV.txt'
+        filename = f'../../data/{quote}_{interval}_OHLCV.txt'
 
         if os.path.isfile(filename) == False:
             print(f'could not source {quote} data')
@@ -16,11 +24,31 @@ def get_data(currencies):
 
     return data
 
-def hs1_sym(currencies, prof_goal):
-    data = get_data(currencies)
+def process_data(data):
+    processed_data = []
+    for i in range(len(data)):
+        s1_data = str(data[i]).split(",")
+        s2_data = []
+        for j in range(len(s1_data)):
+            if j % 6 == 0 and j != 0:
+                s2_data.append(s1_data[j-6:j])
+                print(s1_data[j-6:j])
+
+            # for k in range(len(s2_data)):
+            #     s2_data[k] = getNum(s2_data[k])
+
+            # print(s2_data, "\n")
 
 
-    print(data)
+def hs1_sym(start_cap, currencies, interval, prof_goal):
+    data = get_data(currencies, interval)
+    data = process_data(data)
+    # for i in range(len(currencies)):
+    #     idv_invst = start_cap / len(currencies)
+    #     print(f'sym_buying {idv_invst} worth of {currencies[i]}')
+    #     for i in range(len(str(data[i]))
+
+    #print(str(data[0]).split(",")[0])
     #return(prof, trades,
 
 
@@ -28,8 +56,10 @@ def hs1_sym(currencies, prof_goal):
 
 
 currencies = ['ETH/BTC', 'LTC/BTC', 'BNB/BTC', 'NEO/BTC', 'BCH/BTC', 'GAS/BTC', 'HSR/BTC', 'MCO/BTC', 'WTC/BTC', 'LRC/BTC', 'QTUM/BTC', 'YOYOW/BTC', 'OMG/BTC', 'ZRX/BTC', 'STRAT/BTC', 'SNGLS/BTC', 'BQX/BTC', 'KNC/BTC', 'FUN/BTC', 'SNM/BTC', 'IOTA/BTC', 'LINK/BTC', 'XVG/BTC', 'SALT/BTC', 'MDA/BTC', 'MTL/BTC', 'SUB/BTC', 'EOS/BTC', 'SNT/BTC', 'ETC/BTC', 'MTH/BTC', 'ENG/BTC', 'DNT/BTC', 'ZEC/BTC', 'BNT/BTC', 'AST/BTC', 'DASH/BTC', 'OAX/BTC', 'ICN/BTC', 'BTG/BTC', 'EVX/BTC', 'REQ/BTC', 'VIB/BTC', 'TRX/BTC', 'POWR/BTC', 'ARK/BTC', 'XRP/BTC', 'MOD/BTC', 'ENJ/BTC', 'STORJ/BTC', 'VEN/BTC', 'KMD/BTC', 'RCN/BTC', 'NULS/BTC', 'RDN/BTC', 'XMR/BTC', 'DLT/BTC', 'AMB/BTC', 'BAT/BTC', 'BCPT/BTC', 'ARN/BTC', 'GVT/BTC', 'CDT/BTC', 'GXS/BTC', 'POE/BTC', 'QSP/BTC', 'BTS/BTC', 'XZC/BTC', 'LSK/BTC', 'TNT/BTC', 'FUEL/BTC', 'MANA/BTC', 'BCD/BTC', 'DGD/BTC', 'ADX/BTC', 'ADA/BTC', 'PPT/BTC', 'CMT/BTC', 'XLM/BTC', 'CND/BTC', 'LEND/BTC', 'WABI/BTC', 'TNB/BTC', 'WAVES/BTC', 'GTO/BTC', 'ICX/BTC', 'OST/BTC', 'ELF/BTC', 'AION/BTC', 'NEBL/BTC', 'BRD/BTC', 'EDO/BTC', 'WINGS/BTC', 'NAV/BTC', 'LUN/BTC', 'TRIG/BTC', 'APPC/BTC', 'VIBE/BTC', 'RLC/BTC', 'INS/BTC', 'PIVX/BTC', 'IOST/BTC', 'CHAT/BTC', 'STEEM/BTC', 'XRB/BTC', 'VIA/BTC', 'BLZ/BTC', 'AE/BTC', 'RPX/BTC', 'NCASH/BTC', 'POA/BTC', 'ZIL/BTC', 'ONT/BTC', 'STORM/BTC', 'XEM/BTC', 'WAN/BTC', 'WPR/BTC', 'QLC/BTC', 'SYS/BTC', 'GRS/BTC', 'CLOAK/BTC', 'GNT/BTC', 'LOOM/BTC', 'BCN/BTC', 'REP/BTC', 'TUSD/BTC', 'ZEN/BTC', 'SKY/BTC', 'CVC/BTC', 'THETA/BTC', 'IOTX/BTC', 'QKC/BTC', 'AGI/BTC', 'NXS/BTC', 'DATA/BTC', 'SC/BTC']
+Tinterval= "5m"
+start_cap = 1
 
-hs1_sym(currencies, 1)
+hs1_sym(start_cap, currencies, Tinterval, 0.01)
 
 #hs1 should go through every currency available, buy a bag, wait fon an x% gain in price, and sell that position
 #hstf1 should simulate that ^ process on historical data and record metrics (namely profitability)
